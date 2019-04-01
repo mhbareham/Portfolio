d2d91c7a24b6f27f964e7deaaa10bef327e15f5a
Matthew Abraham (s5116305) <s5116305@mac38566.mac.bournemouth.ac.uk>
2019-04-01 10:29:32 +0100

    Revert "Clean up"
    This reverts commit fbbe8a973e22611630d095d908267de5d057833d.

--- Portfolio/Portfolio/ViewController.swift
+++ Portfolio/Portfolio/ViewController.swift
@@ -5,6 +5,16 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+<<<<<<< HEAD
+
+=======
+    // deez nuts
+>>>>>>> bd71294ba21c22a8f3a0ab08ca0b3ce5bc213cfa
+  }
+
+    func heyYoWhatsUp () {
+        
     }
+
 }
 
