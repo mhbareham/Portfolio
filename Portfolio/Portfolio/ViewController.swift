c4818f9f85040677bdf5cab5f96d9eabb502c5a8
Matthew Abraham (s5116305) <s5116305@mac38566.mac.bournemouth.ac.uk>
2019-04-01 10:22:04 +0100

    Hey yo whats up

--- Portfolio/Portfolio/ViewController.swift
+++ Portfolio/Portfolio/ViewController.swift
@@ -5,9 +5,12 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
+    func heyYoWhatsUp () {
+        
+    }
 
 }
 
