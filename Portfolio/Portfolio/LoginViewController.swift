8d68a4746a4cb9ad6662cf27feb983ccb14b8dc5
Matthew Abraham (s5116305) <s5116305@mac42948.student.bournemouth.ac.uk>
2019-04-04 12:41:11 +0100

    Start of user setup
    Fixed Outlets
    Added Storyboard containers
    New View Controller

--- Portfolio/Portfolio/LoginViewController.swift
+++ Portfolio/Portfolio/LoginViewController.swift
@@ -3,6 +3,14 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    @IBAction func login(_ sender: Any) {
+    }
+
+    
     override func viewDidLoad() {
     super.viewDidLoad()
     }
