d03427503921d0a77a13abf275b0f0ff1c17450c
Matthew Abraham (s5116305) <s5116305@mac38572.mac.bournemouth.ac.uk>
2019-04-29 09:59:42 +0100

    Sign up functionality
    lotsa stuff

--- Portfolio/Portfolio/LoginViewController.swift
+++ Portfolio/Portfolio/LoginViewController.swift
@@ -6,14 +6,43 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-
+    @IBAction func signUp(_ sender: Any) {
+        Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { [unowned self] result, error in
+            
+            guard let user = result?.user else {
+                
+                self.handleError(error)
+                if error != nil{
+                    print("Error Signing Up")
+                }
+                return
+            }
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    func handleError(_ error: Error?) {
+        guard let error = error else {
+            print("dunno what went wrong")
+            return
+        }
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        emailTextField.textColor = .red
+    }
+    
+    
     @IBAction func login(_ sender: Any) {
         guard let email = emailTextField.text, let password = passwordTextField.text else { return }
         
         Auth.auth().signIn(withEmail: email, password: password) { user, error in
-            if let _ = user {
-                self.dismiss(animated: true, completion: nil )
+            guard let _ = user else {
+                self.handleError(error)
+                return
             }
+            self.dismiss(animated: true, completion: nil )
         }
     }
 
