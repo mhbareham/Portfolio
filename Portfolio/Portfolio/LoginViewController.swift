c353f88a54b765e68069b8651ebf2797c716fa91
Matthew Abraham (s5116305) <s5116305@mac42948.mac.bournemouth.ac.uk>
2019-04-30 13:16:15 +0100

    Upload Profile IMage
    doesn't work yet, v. confusing. commented out

--- Portfolio/Portfolio/LoginViewController.swift
+++ Portfolio/Portfolio/LoginViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Firebase
+import FirebaseStorage
 
 class LoginViewController: UIViewController {
     
@@ -19,13 +20,44 @@ class LoginViewController: UIViewController {
             }
             self.dismiss(animated: true, completion: nil)
         }
-        
     }
     
-    func uploadProfileImage(_ image:UIImage, completion: @escaping ((_ url:String?)->())){
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-        let storageRef = Storage.storage().reference().child("user/\(uid)")
+    @IBAction func login(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            guard let _ = user else {
+                self.handleError(error)
+                return
+            }
+            self.dismiss(animated: true, completion: nil )
+        }
     }
+   
+    
+//    func uploadProfileImage(_ image:UIImage, completion: @escaping ((_ url:String?)->())){
+//        guard let uid = Auth.auth().currentUser?.uid else { return }
+//        let storageRef = Storage.storage().reference().child("user/\(uid)")
+//        
+//        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+//        
+//        let metaData = StorageMetadata()
+//        metaData.contentType = "image/jpeg"
+//        
+//        storageRef.putData(imageData, metadata: metaData) { metaData, error in
+//            if error == nil, metaData != nil {
+//                if let url = metaData?.downloadURL()?.absoluteString {
+//                    completion(url)
+//                } else {
+//                    completion(nil)
+//                }
+//                //success!!!!!!
+//            } else {
+//                //fail :(
+//                completion(nil)
+//            }
+//        }
+//    }
     
     func handleError(_ error: Error?) {
         guard let error = error else {
@@ -39,16 +71,6 @@ class LoginViewController: UIViewController {
     }
     
     
-    @IBAction func login(_ sender: Any) {
-        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
-        
-        Auth.auth().signIn(withEmail: email, password: password) { user, error in
-            guard let _ = user else {
-                self.handleError(error)
-                return
-            }
-            self.dismiss(animated: true, completion: nil )
-        }
-    }
+
 
 }
