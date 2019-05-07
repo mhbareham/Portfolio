87da2fa722ca67ab105c406826192e35560ceb22
Matthew Abraham (s5116305) <s5116305@mac38566.mac.bournemouth.ac.uk>
2019-05-07 13:35:05 +0100

    Attempting to create a Post array

--- Portfolio/Portfolio/MainViewController.swift
+++ Portfolio/Portfolio/MainViewController.swift
@@ -7,24 +7,4 @@ class MainViewController: UIViewController {
     @IBAction func signOut(_ sender: Any) {
         AppManager.shared.logout()
     }
-    
-    @IBAction func uploadPhoto(_ sender: Any) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.mediaTypes = [kUTTypeImage as String]
-        imagePicker.delegate = self
-        present(imagePicker, animated: true, completion: nil)
-    }
-}
 
-
-extension MainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.originalImage] as! UIImage
-        //upload(image)
-        dismiss(animated:true, completion: nil)
-    }
-}
